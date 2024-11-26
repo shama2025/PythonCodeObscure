2a70ae4ef809fc1bda092b86f9a0377995e1771d
shama2025 <2003mashaffer@gmail.com>
2024-11-26 09:49:29 -0500

    Update format.py to include comment randomization

--- format.py
+++ format.py
@@ -1,4 +1,5 @@
 import os
+import re
 import random
 
 def scrape_cwd():
@@ -36,15 +37,33 @@ def formatter(python_code):
     """This function will feature formating the document and return a newly formated file"""
 
     idx = 1
+    shuffled_comments = []
     for element in python_code:
-        if "#" in element or '"""' in element: # replaces the comments with pirate text
-            "Need to find way to isolate only the line containing the comment"
-            # Can slice the string using the # or """ and """ or \n
+        if "#" in element or '"""' in element:
             split_elements = element.split('\n')
             for element in split_elements:
-                if "#" not in element or '"""' not in element:
+                if "#" not in element and '"""' not in element:
                     split_elements.pop(split_elements.index(element))
-            print(split_elements)
+
+            unshuffled_comments = [re.sub(r'\bprint\s*\([^)]*\)', '', element) for element in split_elements]
+
+            for comment in unshuffled_comments:
+                list_comment = list(comment.replace(" ", ""))
+                if "#" in comment: # look at alternative ways to shuffle the text will need to implement ai to shuffle the text
+                    """str + list + str"""
+                    shuffled_comments.append(list_comment[0] + "".join(random.sample(list_comment[1:-2],len(list_comment[1:-2]))) + "".join(list_comment[-1:]))
+                else:
+                    #print(type(list_comment[0:2]) , type(random.sample(list_comment[3:-4],len(list_comment[3:-4]))) , type(list_comment[-3]))
+                    """list + list + str"""
+                    shuffled_comments.append("".join(list_comment[0:2]) + "".join(random.sample(list_comment[3:-4],len(list_comment[3:-4]))) + "".join(list_comment[-3:]))
+
+            print(shuffled_comments)
+
+        # now put the comments back into the code
+        
+        # make a section that focuses on variable names and converting those
+        if '=' in element:
+            "Swap variable names with names of fruit"
         if "def" in element: # formats function declaration to be exec("""""")
             python_code[idx]= 'exec("""'+ element + '""")'
 
